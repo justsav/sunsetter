491ab159623a5b50e89d80381e4cc9edb1fd55c0
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-26 12:07:00 +0000

    added cities controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'cities/show'
+  get 'cities/index'
+  root to: 'pages#home'
 
   devise_for :users
-  root to: 'pages#home'
+  resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
