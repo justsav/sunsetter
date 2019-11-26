83636c6f9f3e2c77a0ba8b51ea10ac9864d03490
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-26 13:40:49 +0000

    added city search params

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-
-  root to: 'cities#index'
+  root to: 'pages#home'
 
   devise_for :users
-  resources :places
+  resources :cities, only: [:index] do
+    resources :places
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  resources :sunsets, only: [:index]
+
 end
