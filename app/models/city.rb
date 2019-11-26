18aa1aa3dae169b20d537f1310757f660d2f22e0
Justin Savage <justin.savage@live.com>
2019-11-26 13:43:23 +0000

    connected dependency between city and place

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,3 @@
 class City < ApplicationRecord
-  has_many :places
+  has_many :places, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_26_114719) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +23,17 @@ ActiveRecord::Schema.define(version: 2019_11_26_114719) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "places", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.string "address", null: false
+    t.string "image", null: false
+    t.bigint "city_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city_id"], name: "index_places_on_city_id"
+  end
+
   create_table "sunsets", force: :cascade do |t|
     t.time "start", null: false
     t.time "end", null: false
@@ -35,17 +44,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_114719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_sunsets_on_city_id"
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.string "address", null: false
-    t.string "image", null: false
-    t.bigint "city_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["city_id"], name: "index_places_on_city_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,9 +60,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_114719) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "places", "cities"
-
   add_foreign_key "sunsets", "cities"
-
 end
