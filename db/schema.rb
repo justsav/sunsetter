07c4ed66c8b52db319c4af4a89a31b22e9014784
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-26 11:28:07 +0000

    created cities table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_164630) do
+ActiveRecord::Schema.define(version: 2019_11_26_112724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "days", force: :cascade do |t|
     t.string "date", null: false
