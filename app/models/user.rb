e392877bdc07b4e74f47e8a1993bf3128ff9b8dc
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-26 17:41:20 +0000

    added reviews controller/table/model/routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
+  has_many :reviews
   validates :first_name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
 
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destroy'
   root to: 'pages#home'
 
   devise_for :users
@@ -10,5 +17,8 @@ Rails.application.routes.draw do
   end
   resources :sunsets, only: [:index]
   resources :places, except: [:index]
-  resources :bookings
+  resources :bookings do
+    resources :reviews, only: [:index]
+  end
+  resources :reviews, except: [:index]
 end
