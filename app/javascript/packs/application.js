7d3b95ba9ce604ce8f2af8550edab930e3692863
Nick Galluzzo <n.galluzzo@gmail.com>
2019-11-27 17:05:38 +0000

    added map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
 
 var SunCalc = require('suncalc');
 var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
 console.log(times);
+
+initMapbox();
