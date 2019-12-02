6566161464bb07b28df5480be07fafae1b1890d8
Justin Savage <justin.savage@live.com>
2019-12-02 19:16:09 +0000

    improved map and have reviews showing up

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :booking
   belongs_to :user
   mount_uploader :image, PhotoUploader
+
+  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (1..5).to_a }
 end
